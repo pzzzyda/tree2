ec060a25783ad3c2cb1e0b8ce07464c87e5ec864
pzzzyda <zhongyuan03@outlook.com>
2025-11-25 18:25:38 +0800

    refactor: refactor command line arguments
    Rename some command line arguments. Add more detailed help messages for
    each argument.

--- src/app.rs
+++ src/app.rs
@@ -9,27 +9,50 @@ use crate::fmt::print_tree_with_config;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 pub struct TreeArgs {
+    #[arg(required = true, help = "Path to the directory to display")]
     path: String,
 
-    #[arg(short, long, default_value_t = false)]
+    #[arg(
+        short,
+        long,
+        default_value_t = false,
+        help = "Show hidden files and directories"
+    )]
     all: bool,
 
-    #[arg(short, long)]
-    max_depth: Option<usize>,
+    #[arg(long, help = "Limit the depth of the directory tree to display")]
+    level: Option<usize>,
 
-    #[arg(short = 'd', long, default_value_t = false)]
+    #[arg(
+        short,
+        long,
+        default_value_t = false,
+        help = "Show only directories, not files"
+    )]
     directories_only: bool,
 
-    #[arg(long, default_value_t = false)]
+    #[arg(long, default_value_t = false, help = "Disable colorized output")]
     no_color: bool,
 
-    #[arg(long, default_value_t = false)]
+    #[arg(
+        long,
+        default_value_t = false,
+        help = "Use ASCII characters only for tree display"
+    )]
     ascii: bool,
 
-    #[arg(long, default_value_t = false)]
-    sort: bool,
+    #[arg(
+        long,
+        default_value_t = false,
+        help = "Disable sorting of directory entries"
+    )]
+    no_sorting: bool,
 
-    #[arg(long, default_value_t = false)]
+    #[arg(
+        long,
+        default_value_t = false,
+        help = "Show hidden files and directories (ignores .gitignore)"
+    )]
     show_gitignore: bool,
 }
 
@@ -37,12 +60,12 @@ pub fn run_tree_command(args: &TreeArgs) -> Result<(), TreeError> {
     let path = Path::new(args.path.as_str());
 
     let config = TreeConfig {
-        max_depth: args.max_depth,
+        max_depth: args.level,
         show_hidden: args.all,
         dirs_only: args.directories_only,
         with_color: !args.no_color,
         ascii_only: args.ascii,
-        sort: args.sort,
+        sort: !args.no_sorting,
         ignore_gitignore: !args.show_gitignore,
     };
 
